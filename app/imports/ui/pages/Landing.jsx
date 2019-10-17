2379956025d920483780f58d6bd7ef15d0a26db2
Ken Tung <kentung@hawaii.edu>
2019-10-17 13:43:13 -1000

    Basic UI Layout

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+        <Grid verticalAlign='middle' textAlign='middle' container>
+          <Grid.Column width={20} textAlign='left'>
+            <h1>What are your diving plans for today?</h1>
+            <form className="ui form">
+              <div className="inline field">
+                <label>Depth</label>
+                <div className="ui input"><input type="text" placeholder="feet"/></div>
+              </div>
+            </form>
+            <form className="ui form">
+              <div className="inline field">
+                <label>Time</label>
+                <div className="ui input"><input type="text" placeholder="minutes"/></div>
+              </div>
+            </form>
+            <form className="ui form">
+              <div className="inline field">
+                <label>Planned Surface Interval</label>
+                <div className="ui input"><input type="text" placeholder="minutes"/></div>
+              </div>
+            </form>
+            <button className="ui button">Submit</button>
           </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
         </Grid>
     );
   }
