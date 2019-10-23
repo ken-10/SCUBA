d552c15e138098f4b5213441fd18abed3de57f11
Ken Tung <kentung@hawaii.edu>
2019-10-22 20:02:56 -1000

    Changed AddDive to AddStuff

--- app/imports/ui/pages/AddStuff.jsx
+++ app/imports/ui/pages/AddStuff.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Dive, DiveSchema } from '/imports/api/stuff/stuff';
-import { Grid, Segment, Header, Container } from 'semantic-ui-react';
+import { Stuffs, StuffSchema } from '/imports/api/stuff/stuff';
+import { Grid, Segment, Header } from 'semantic-ui-react';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
+import NumField from 'uniforms-semantic/NumField';
+import SelectField from 'uniforms-semantic/SelectField';
 import SubmitField from 'uniforms-semantic/SubmitField';
 import HiddenField from 'uniforms-semantic/HiddenField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
@@ -10,7 +12,7 @@ import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
 
 /** Renders the Page for adding a document. */
-class AddDive extends React.Component {
+class AddStuff extends React.Component {
 
   /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
   constructor(props) {
@@ -32,35 +34,31 @@ class AddDive extends React.Component {
 
   /** On submit, insert the data. */
   submit(data) {
-    const { diveDepth, diveTime, surfaceInterval } = data;
+    const { name, quantity, condition } = data;
     const owner = Meteor.user().username;
-    Dive.insert({ diveDepth, diveTime, surfaceInterval, owner },
-        this.insertCallback);
+    Stuffs.insert({ name, quantity, condition, owner }, this.insertCallback);
   }
 
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
-    const divStyle = { paddingBottom: '25px' };
     return (
-        <Container style={divStyle}>
-          <Grid container centered>
-            <Grid.Column>
-              <Header as="h2" textAlign="center">Add Diving Info</Header>
-              <AutoForm ref={(ref) => { this.formRef = ref; }} schema={DiveSchema} onSubmit={this.submit}>
-                <Segment>
-                  <TextField name='diveDepth'/>
-                  <TextField name='diveTime'/>
-                  <TextField name='surfaceInterval'/>
-                  <SubmitField value='Submit'/>
-                  <ErrorsField/>
-                  <HiddenField name='owner' value='fakeuser@foo.com'/>
-                </Segment>
-              </AutoForm>
-            </Grid.Column>
-          </Grid>
-        </Container>
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Add Stuff</Header>
+            <AutoForm ref={(ref) => { this.formRef = ref; }} schema={StuffSchema} onSubmit={this.submit}>
+              <Segment>
+                <TextField name='name'/>
+                <NumField name='quantity' decimal={false}/>
+                <SelectField name='condition'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+                <HiddenField name='owner' value='fakeuser@foo.com'/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
     );
   }
 }
 
-export default AddDive;
+export default AddStuff;
