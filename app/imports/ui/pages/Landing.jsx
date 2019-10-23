d399277aacd7a88f5d6f5bd65af98486345a84ad
Ken Tung <kentung@hawaii.edu>
2019-10-22 19:32:57 -1000

    Implemented Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,34 +3,27 @@ import { Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
+
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='middle' container>
-          <Grid.Column width={20} textAlign='left'>
-            <h1>What are your diving plans for today?</h1>
-            <form className="ui form">
-              <div className="inline field">
-                <label>Depth</label>
-                <div className="ui input"><input type="text" placeholder="feet"/></div>
-              </div>
-            </form>
-            <form className="ui form">
-              <div className="inline field">
-                <label>Time</label>
-                <div className="ui input"><input type="text" placeholder="minutes"/></div>
-              </div>
-            </form>
-            <form className="ui form">
-              <div className="inline field">
-                <label>Planned Surface Interval</label>
-                <div className="ui input"><input type="text" placeholder="minutes"/></div>
-              </div>
-            </form>
-            <button className="ui button">Submit</button>
-          </Grid.Column>
-        </Grid>
+        <div className='landing-background'>
+          <Grid container stackable centered columns ={1}>
+            <Grid.Column textAlign={'center'}>
+              <Grid.Row className="title-main">
+                <p className="main-text">
+                  Welcome to Dive Planner!
+                </p>
+                <p className="sub-main-text">
+                    Your online dive planner!
+                </p>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
+
+
 }
 
 export default Landing;
