ffe1c47df4f24c00902cf8566c1426155b4ba0f8
Ken Tung <kentung@hawaii.edu>
2019-11-22 15:21:42 -1000

    added register button. fixing animation still

--- app/imports/ui/components/DefaultLanding.jsx
+++ app/imports/ui/components/DefaultLanding.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Grid, Header, Transition, Image } from 'semantic-ui-react';
+import { Grid, Header, Transition, Image, Button, Icon } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react/dist/commonjs/modules/Dropdown';
+import { NavLink } from 'react-router-dom';
 
 class DefaultLanding extends React.Component {
   state = { visible: true }
@@ -9,6 +11,15 @@ class DefaultLanding extends React.Component {
 
   render() {
      const { visible } = this.state;
+     const style = {
+       background: '#FBF1D4',
+       paddingTop: '40px',
+       paddingBottom: '40px',
+       paddingLeft: '35px',
+       paddingRight: '35px',
+
+     };
+
 
     return (
         <div className="landing-page" onLoad= 'toggleVisibility();'>
@@ -23,7 +34,14 @@ class DefaultLanding extends React.Component {
                 <p className="landing-text">
                   <b>Less</b> time with tables, <b>More</b> time diving
                 </p>
+                  <Button animated='fade'>
+                    <Button.Content visible>Register Now</Button.Content>
+                    <Button.Content hidden className="button content">
+                      It's free!
+                    </Button.Content>
+                  </Button>
               </Grid.Column>
+
               <Grid.Column width={8}>
                 <div className="landing-img-1">
                   <Image style={{ 'font-size': 171 }} avatar src='/images/landing-scuba.svg'/>
@@ -33,14 +51,24 @@ class DefaultLanding extends React.Component {
           </Grid>
           </div>
 
-          <Grid divided='vertically' textAlign='center' style={{ marginTop: '0' }}>
-            <Grid.Row columns={1} className="landing-panel-2">
+          <Grid style={{ marginTop: '0' }} textAlign='center' >
+            <Grid.Row columns={1} className="landing-panel-2" style={{ height: '40px' }}>
               <Grid.Column>
               <Header as={'landing-header3'}>
-                What is <i>my</i>Diver?<br/>
+                <b>What is </b>
+                <Header as={'landing-header-sub-3'}>
+                  <b><i>my</i>Diver?</b></Header>
+                <br/>
               </Header>
               </Grid.Column>
             </Grid.Row>
+
+            <Grid.Row columns={1} style={{ height: '43px' }}>
+              <div className="landing-divider">
+                  <p>__________</p>
+              </div>
+            </Grid.Row>
+
               <Grid.Row columns={1}>
                 <Grid.Column width={8}>
                 <p className="landing-text2"><i>my</i>Diver is an online dive table and journal. Below are some of our features listed:<br/>
@@ -48,8 +76,57 @@ class DefaultLanding extends React.Component {
                 </Grid.Column>
               </Grid.Row>
 
+            <Grid.Row className="landing-card-row" columns={3}>
+              <Grid.Column width={4}>
+                <a className="ui card" style={{ height: '400px', width: '250px' }}>
+                  <div className="content" style={style}>
+                    <Header as={'landing-card-header'}>Save <b>Time</b></Header>
+                    <div className="description">
+                      <br/>
+                      Calculate all the times you need for your dive in minutes so you can dive as soon as possible.
+                      <br/><br/><br/><br/>
+                    </div>
+                    <div className="card-img">
+                    <Image height="70" width="70" src='/images/icon-alarm.png'/>
+                    </div>
+                  </div>
+                </a>
+              </Grid.Column>
+
+              <Grid.Column width={4}>
+                <a className="ui card" style={{ height: '400px', width: '250px' }}>
+                  <div className="content" style={style}>
+                    <Header as={'landing-card-header'}> <b>Easy</b> Interface</Header>
+                    <div className="description">
+                      <br/>
+                      Log in and get planning. A simple interface for anyone to understand and use.<br/><br/><br/><br/>
+                    </div>
+                    <div className="card-img-2">
+                      <Image width="75" height="75" src='/images/pointer.png'/>
+                    </div>
+                  </div>
+                </a>
+              </Grid.Column>
+
+              <Grid.Column width={4}>
+                <a className="ui card" style={{ height: '400px', width: '250px' }}>
+                  <div className="content" style={style}>
+                    <Header as={'landing-card-header'}> <b>Track</b> Dives</Header>
+                    <div className="description">
+                      <br/>
+                      Customizable dive log that logs dives right after submission.<br/><br/><br/><br/>
+                    </div>
+                    <div className="card-img-3">
+                      <Image width="75" height="75" src='/images/stats.png'/>
+                    </div>
+                  </div>
+                </a>
+              </Grid.Column>
+            </Grid.Row>
+
+
           </Grid>
-        </div>
+    </div>
 
     );
   }
