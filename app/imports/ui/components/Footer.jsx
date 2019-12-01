3be8ee2a5edc7be8815da5acc988f389f2e322cc
Ken Tung <kentung@hawaii.edu>
2019-11-30 16:58:54 -1000

    styling updates, and new footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
+  siteRedirect() {
+    const url = 'https://github.com/ken-10';
+    window.open(url, '_blank');
+  }
+  
+  siteRedirect2() {
+    const url = 'https://github.com/jhunDomingo';
+    window.open(url, '_blank');
+  }
+
+
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '3%', paddingBottom: '7%' };
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
+        <footer style={{ height: '100%', backgroundColor: '#475F6F', color: 'whitesmoke' }}>
+          <div style={divStyle} className="ui fluid center aligned container">
+            <hr/>
+            <div>
+              <Button circular icon={'github'} className={'footerButton'} onClick={this.siteRedirect}/>
+              <Button circular icon={'github'} className={'footerButton'} onClick={this.siteRedirect2}/>
+            </div>
+            <div className="footer-text">
+              myDiver • University of Hawaiʻi at Mānoa • 2500 Campus Road • Honolulu, HI 96822<br/>
+              DISCLAIMER: myDiver should not be used in any real life scenario. It is purely a software project for ICS
+              414 at University of Hawaiʻi at Mānoa
+            </div>
           </div>
         </footer>
     );
